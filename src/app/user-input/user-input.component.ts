d57b58fc40dc1a7e4e1411e631a84b98951cc948
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-19 14:35:28 -0400

    Add user-input component

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    console.log('Submitted!');
+    console.log(
+      this.enteredInitialInvestment,
+      this.enteredAnnualInvestment,
+      this.enteredExpectedReturn,
+      this.enteredDuration
+    );
+  }
+}
