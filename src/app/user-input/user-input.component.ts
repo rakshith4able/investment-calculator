bbd0a6b8f555dc95ce45eceaf27c0872c531dca8
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-19 20:59:19 -0400

    Convert standalone to modules

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,9 @@
 import { Component, EventEmitter, output, Output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  imports: [FormsModule],
+  standalone: false,
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
